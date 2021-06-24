857bca36b1703044ee347f23c71cf5c21a6d74cb
Hriman Mahanta <66152308+Hriman-Mahanta@users.noreply.github.com>
2021-06-24 12:12:57 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>About Us</title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <script type="text/javascript" src="bootstrap/js/jquery.js"></script>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <?php
+        include "includes/header.php";
+        ?>
+        <div class="container ">
+            <div class="margin_row">
+                <div class="col-xs-6">
+                    <h2>Who are we?</h2>
+                    <p>We are a group of young technocrats who came up with an idea of solving budget and<br>
+                        time issues which we usually face in our daily lives. We are here to provide a budget<br>
+                        controller according to your aspects.
+                    </p>
+                    <p>Budget control is the biggest financial issue in the present world. One should look after<br>
+                       their budget control to get ride off from their financial crisis.
+                    </p>
+                    <h2>Contact Us</h2>
+                    <p><b>Email: </b>trainings@internshala.com</p>
+                    <p><b>Mobile: </b>+91-8448444853</p>
+                </div>
+                <div class="col-xs-6">
+                    <h2>Why choose us?</h2>
+                    <p>We provide with a predominant way to control and manage your budget estimations with
+                       ease of accessing for multiple users.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <?php
+        include "includes/footer.php"
+        ?>
+    </body>
+</html>
