514709a7316b73921e84c03c706de0b394a22f7d
Hriman Mahanta <66152308+Hriman-Mahanta@users.noreply.github.com>
2021-06-24 12:14:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>CTRL Budget</title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <script type="text/javascript" src="bootstrap/js/jquery.js"></script>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    
+    <body>
+        <?php
+        include "includes/header.php";
+        ?>
+        <div id="banner_image">
+            <div class="container">
+                <center>
+                <div id="banner_content">
+                    <h1>We help you control your budget</h1>
+                    <a href="login.php" class="btn btn-info btn-lg active">Start Today</a>
+                </div>
+                </center>
+            </div>
+        </div>
+        <?php
+        include "includes/footer.php"
+        ?>
+    </body>
+</html>
